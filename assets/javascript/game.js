8938582fc98ee03e54f3c72fc03a91b75f351493
Somin Kang <somin.kang.91@gmail.com>
2020-04-24 14:52:34 -0700

    update work form last two days

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,41 +1,59 @@
-var letters = ["a","b","c"]
-
+//variables
+var letters = ["a","b","c"];
 var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
+var guessesSoFar = [];
 
-
-// what is this for
-var guessedLetters = [];
-
-// check this
-var playerGuesses = null;
+var letterToGuess = null;
 
 //link to html Guesses Left
 var updateGuessesLeft = function() {
     document.querySelector("#guessesLeft").innerHTML = guessesLeft;
-}
+};
 
 //link to html Guesses So Far
 var updateGuessesSoFar = function () {
-    document.querySelector("#guessesSoFar").innerHTML = guessedLetters.join(", ");
-}
+    document.querySelector("#guessesSoFar").innerHTML = guessesSoFar.join(", ");
+};
 
-var updatePlayerGuesses = function () {
-    playerGuesses = letters[Math.floor(Math.random()*letters.length)];
-}
+//pick random number from string
+var updateLetterToGuess = function () {
+    letterToGuess = letters[Math.floor(Math.random() * letters.length)];
+};
 
+
+//what is this for?
+//updateLetterToGuess();
+//updateGuessesLeft();
+
+
+//review this
+document.onkeydown = function() {
+    guessesLeft--;
+    var letters = event.key.toLowerCase();
+    guessesSoFar.push(letters);
+    updateGuessesLeft();
+    updateGuessesSoFar();
+
+    if (letters === letterToGuess) {
+        wins++;
+        document.querySelector("#wins").innerHTML = wins;
+        reset();
+    }
+
+    if (guessesLeft === 0) {
+        losses++;
+        document.querySelector("#losses").innerHTML = losses;
+        reset();
+    }
+};
 
 //reset function
 var reset = function() {
     guessesLeft = 9;
-    guessedLetters = [];
+    guessesSoFar = [];
     updateGuessesLeft();
     updateGuessesSoFar();
-    updatePlayerGuesses();
-}
-
-//This from google books
-//Define the Function - fundtion letterGuess() {}
-//Retrieve Guess - funtion letterGuess() { var playerGuess = document.getElementByID("guess")}
-//Test and Update Loop - for (var L = 0; L<secretName.lenght;L++)}
+    updateLetterToGuess();
+};
